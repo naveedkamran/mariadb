754f73b1054368365c208ce8c725f47facb6d20a
Naveed Kamran <naveedkamran4@gmail.com>
2019-11-13 08:31:17 -0800

    Modifications for AWS centos machine

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ansible-playbook --flush-cache -i hosts-prod "roles/base/tasks/main.yml"
-ansible-playbook --flush-cache -i hosts-prod "roles/docker/tasks/main.yml"
-ansible-playbook --flush-cache -i hosts-prod "roles/db-maria/tasks/main.yml"
+ansible-playbook --flush-cache -i hosts-aws "roles/base/tasks/main.yml"
+ansible-playbook --flush-cache -i hosts-aws "roles/docker/tasks/main.yml"
+ansible-playbook --flush-cache -i hosts-aws "roles/db-maria/tasks/main.yml"
